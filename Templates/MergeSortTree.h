655b03805c5a7261f0bf196c050aa77126bc792f
Αναστάσης Κολιόπουλος <ice22390097@uniwa.gr>
2026-04-04 02:50:10 +0300

    Added a Lambda function.

--- Templates/MergeSortTree.h
+++ Templates/MergeSortTree.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <functional>
 
 template<typename T>
 class MergeSortTree
@@ -70,17 +71,7 @@ private:
 		std::merge(current->left->A.begin(), current->left->A.end(), current->right->A.begin(), current->right->A.end(), std::back_inserter(current->A));
 	}
 
-	/// <summary>
-	/// Recursively finds the number of elements greater than k in the segment [qlo, qhi].
-	/// </summary>
-	/// <param name="k">The element whose number of larger elements is being computed.</param>
-	/// <param name="qlo">First index of the query range.</param>
-	/// <param name="qhi">Last index of the query range.</param>
-	/// <param name="current">Current node of the tree.</param>
-	/// <param name="lo">First index of the segment that this node contains.</param>
-	/// <param name="hi">Last index of the segment that this node contains.</param>
-	/// <returns>The number of elements greater than k in the segment [qlo, qhi].</returns>
-	int FindNumberOfGreaterElements(const T k, const int qlo, const int qhi, Node* current, int lo, int hi) const
+	int Count(const T k, const int qlo, const int qhi, Node* current, int lo, int hi, std::function<int(const std::vector<T>&, const T)> count) const
 	{
 		// If the current node is null, no elements exist in the current node, so return 0.
 		if (current == nullptr)
@@ -94,23 +85,20 @@ private:
 			return 0;
 		}
 
-		// If the current segment is completely inside the query range, return the number of elements in the current node that are greater than k.
 		if (qlo <= lo && hi <= qhi)
 		{
-			return current->A.size() - (std::upper_bound(current->A.begin(), current->A.end(), k) - current->A.begin());
+			return count(current->A, k);
+			//return current->A.size() - (std::upper_bound(current->A.begin(), current->A.end(), k) - current->A.begin());
 		}
 
 		// Otherwise, split the current segment into two halves.
 		int mid = (lo + hi) / 2;
 
-		// Recursively find the number of greater elements in the left subtree in the query range.
-		int numberOfGreaterElementsLeft = FindNumberOfGreaterElements(k, qlo, qhi, current->left, lo, mid);
+		int countLeft = Count(k, qlo, qhi, current->left, lo, mid, count);
 
-		// Recursively find the number of greater elements in the right subtree in the query range.
-		int numberOfGreaterElementsRight = FindNumberOfGreaterElements(k, qlo, qhi, current->right, mid + 1, hi);
+		int countRight = Count(k, qlo, qhi, current->right, mid + 1, hi, count);
 
-		// Return the total number of greater elements found in both subtrees in the query range.
-		return numberOfGreaterElementsLeft + numberOfGreaterElementsRight;
+		return countLeft + countRight;
 	}
 
 public:
@@ -124,7 +112,7 @@ public:
 	}
 
 	/// <summary>
-	/// Helper function that calls the recursive function to find the number of elements greater than k in the segment [qlo, qhi].
+	///
 	/// </summary>
 	/// <param name="k">The element whose number of larger elements is being computed.</param>
 	/// <param name="qlo">First index of the query range.</param>
@@ -132,7 +120,7 @@ public:
 	/// <returns>The number of elements greater than k in the segment [qlo, qhi].</returns>
 	int FindNumberOfGreaterElements(const T k, const int qlo, const int qhi) const
 	{
-		return FindNumberOfGreaterElements(k, qlo, qhi, root, 0, N - 1);
+		return Count(k, qlo, qhi, root, 0, N - 1, [](const std::vector<T>& A, const T k) { return A.end() - std::upper_bound(A.begin(), A.end(), k); });
 	}
 };
 
